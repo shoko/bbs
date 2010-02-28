d2acf3c10ee456e8eb5d0714d937f1c0a8191211
shoko <shoichi@kokeguchi.com>
2010-02-28 13:43:39 +0900

    a

--- createble.php
+++ createble.php
@@ -3,12 +3,11 @@ include_once("config.php");
 mysql_connect($db_host, $db_name, $db_username, $db_password);
 $dbh = mysql_connect($db_host, $db_name, $db_username, $db_password);
 
-if( $dbh === false ) echo "rrrr";
 $sqlstr = "use $db_name";
-//mysql_select_db($db_name);
-echo "$sqlstr";
+
+//echo "$sqlstr";
 mysql_query($sqlstr, $dbh);
-//if($res === false) echo "B";
+
 $sqlstr = "create table mybbs(".
 "id INT AUTO_INCREMENT PRIMARY KEY,".
 "mailaddr VARCHAR(80),".
